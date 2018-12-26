eea96c5a10993d616a5166a852121732169fe53f
Valdes Duran <aury.valdes.duran@accenture.com>
2018-12-26 14:52:41 -0300

    continuoMenu

--- app/controllers/menu/menuController.js
+++
@@ -1,8 +0,0 @@
-angular.module('menuModule', [])
-.controller('menuCtrl', ['$scope', 'crudService', function($scope, crudService) {
-    $scope.listMenu;
-   
-    crudService.traeAllData().then(function(resp){
-        $scope.listMenu = resp.data;      
-    });
-}])
