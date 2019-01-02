5eebfb7b76acb6f2e8ed7337eded8afbf91c72a0
Valdes Duran <aury.valdes.duran@accenture.com>
2019-01-02 09:26:43 -0300

    "error"Menu

--- app/controllers/menu/menuDirective.js
+++ app/controllers/menu/menuDirective.js
@@ -9,17 +9,20 @@ angular.module('menuModule', [])
 
 .directive('menu', function() {
     return {
-      template: `<!-- Navbar -->
-      <nav class="navbar navbar-inverse menu-nav">
+      template: 
+      `<nav class="navbar navbar-inverse menu-nav">
         <div class="container-fluid menu-div">
           <ul class="nav navbar-nav nav-list">
-            <li><a href="#/app">Home</a></li>
+          <li><a href="#/app">Home</a></li>
             <li ng-repeat="(key, value) in listMenu">
-              <a href="#/app/{{ key }}">{{key}}</a>
+              <a href="#/app/{{key}}">{{key}}</a>
+            </li>
+            <li ng-repeat=" opt in listMenu" ng-bind-html-unsafe="getContent(opt)" >
+              {{ opt.value }}
             </li>
           </ul>
         </div>
-      </nav>
-      <!-- Navbar-end -->`
+      </nav>`
+  
     };
   });
